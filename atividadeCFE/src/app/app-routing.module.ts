e8a3788e22e4f4a2573bf7f453441ebe42e95c0a
Icaro Carvalho <icarogacarvalho@gmail.com>
2024-01-23 13:52:50 -0300

    finalizando o front-end

--- atividadeCFE/src/app/app-routing.module.ts
+++ atividadeCFE/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '', redirectTo:'/inicio', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
